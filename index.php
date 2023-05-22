2e167a2beb4ecd7d667277cb685c8f06438c0a0b
Jack Kimmins <jackkimmins@outlook.com>
2023-05-22 12:42:40 +0100

    Added AddDir

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
+session_start();
+// $_SESSION['role'] = 'admin';
+
 require './jRoute/_load.php';
 
 $route = new jRoute('/jroute', false);
 
 // Register routes
 $route->Route(['get', 'post'], '/', function() {
-    return 'Hello world';
-});
+    return 'Hello, world!';
+}, 'admin');
 
 $route->Route(['get', 'post'], '/test/{id}', "contents/includes/test.php");
 
@@ -18,6 +21,8 @@ $route->Route(['get'], '/user/{id}', function ($userId) {
 $route->Route(['get'], '/user/{id}/{name}', function ($userId, $name) {
     return 'User with id: ' . $userId . ' and name: ' . $name;
 });
+
+$route->AddDir('/public', './contents/public', 'admin');
 
 // Dispatch request
 echo $route->Dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
--- jRoute/_load.php
+++ jRoute/_load.php
@@ -3,6 +3,7 @@
 class jRoute
 {
     private $routes = [];
+    private $dirMappings = [];
     private $urlPrefix = null;
     private $debugMode = false;
 
@@ -17,18 +18,21 @@ class jRoute
         $this->debugMode = $debugMode;
     }
 
-    public function Route($methods, $pattern, $callback)
+    public function Route($methods, $pattern, $callback, $requiredRole = null)
     {
         foreach ($methods as $method) {
             $method = strtoupper($method);
             if (!isset($this->routes[$method])) {
                 $this->routes[$method] = [];
             }
-            $this->routes[$method][$pattern] = $callback;
+            $this->routes[$method][$pattern] = [
+                'callback' => $callback,
+                'role' => $requiredRole
+            ];
         }
-    }
+    }    
 
-    public function PassRoute($baseUrl, $dir)
+    public function PassRoute($baseUrl, $dir, $requiredRole = null)
     {
         $this->Route(['get'], $baseUrl . '{path}', function($path) use ($dir) {
             $file = __DIR__ . $dir . $path;
@@ -38,11 +42,29 @@ class jRoute
                 $_GET['error_uri'] = 'GET ' . $baseUrl . $path;
                 require dirname(__FILE__) . '/errorPages/404.php';
             }
-        });
+        }, $requiredRole);
+    }    
+
+    public function AddDir($webPath, $rootDir, $requiredRole = null)
+    {
+        $rootDir = realpath($rootDir);
+        if ($rootDir === false) {
+            $this->OutputError("Directory does not exist: " . $rootDir);
+            return;
+        }
+    
+        // Store the directory mapping
+        $this->dirMappings[] = [
+            'webPath' => $webPath,
+            'rootDir' => $rootDir,
+            'role' => $requiredRole
+        ];
     }
 
     public function Dispatch($method, $uri)
     {
+        @session_start(); // Start a session
+
         $method = strtoupper($method);
         if (!isset($this->routes[$method])) {
             // Handle method not supported
@@ -52,22 +74,46 @@ class jRoute
         if ($this->urlPrefix != null) $uri = substr($uri, strlen($this->urlPrefix));
         if ($this->debugMode) $this->OutputError($method . ' ' . $uri);
 
-        foreach ($this->routes[$method] as $route => $callback)
+        foreach ($this->routes[$method] as $route => $routeInfo)
         {
             $routePattern = '#^' . preg_replace('/\{([^}]+)\}/', '([^/]+)', $route) . '$#';
             if (preg_match($routePattern, $uri, $matches))
             {
                 array_shift($matches);
 
-                if (is_callable($callback)) return call_user_func_array($callback, $matches);
+                // Check if the user has the required role for this route
+                if ($routeInfo['role'] !== null && (!isset($_SESSION['role']) || $_SESSION['role'] !== $routeInfo['role'])) {
+                    $_GET['error_uri'] = $method . ' ' . $uri;
+                    require dirname(__FILE__) . '/errorPages/403.php'; // Send to a 403 Forbidden error page
+                    return;
+                }
 
-                elseif (is_string($callback)) {
+                if (is_callable($routeInfo['callback'])) return call_user_func_array($routeInfo['callback'], $matches);
+
+                elseif (is_string($routeInfo['callback'])) {
                     preg_match_all('/\{([^}]+)\}/', $route, $paramNames);
                     $paramNames = $paramNames[1];
                     $params = array_combine($paramNames, $matches);
                     $_GET = array_merge($_GET, $params);
-                    require $callback;
+                    require $routeInfo['callback'];
                     return;
+                }
+            }
+        }
+
+        // If route was not found, check directory mappings
+        foreach ($this->dirMappings as $mapping) {
+            if (strpos($uri, $mapping['webPath']) === 0) {
+                // Check if the user has the required role for this route
+                if ($mapping['role'] !== null && (!isset($_SESSION['role']) || $_SESSION['role'] !== $mapping['role'])) {
+                    $_GET['error_uri'] = $method . ' ' . $uri;
+                    require dirname(__FILE__) . '/errorPages/403.php'; // Send to a 403 Forbidden error page
+                    return;
+                }
+
+                $filePath = $mapping['rootDir'] . substr($uri, strlen($mapping['webPath']));
+                if (file_exists($filePath) && !is_dir($filePath)) {
+                    return readfile($filePath);
                 }
             }
         }
@@ -76,4 +122,5 @@ class jRoute
         $_GET['error_uri'] = $method . ' ' . $uri;
         require dirname(__FILE__) . '/errorPages/404.php';
     }
+
 }
